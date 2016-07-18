1ca748ddfa4a8dc854d472f815e948fe503d7a52
Lisda Kania Yuliani <kanialisda@gmail.com>
2016-07-18 15:49:26 +0700

    Modified controller, model, views new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,23 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    flash[:notice] = "Success Add Records"
+    redirect_to action: 'index'
+  end
+
+  def edit
+  end
+  
+  private
+    def params_article
+        params.require(:article).permit(:title, :content, :status)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+
+  validates :title, presence: true,
+                            length: { minimum: 5 }
+  validates :content, presence: true,
+                            length: { minimum: 10 }
+  validates :status, presence: true
 end
